d99ce21fbe83496dd6f14fb51317b57d3e6fef95
YouJungHa <mayongpv@icloud.com>
2021-07-20 00:41:17 +0900

    GroundManager & BlackInfo  - 해설 작성

--- Assets/BlockInfo.cs
+++ Assets/BlockInfo.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum BlockType
+public enum BlockType // 4. 샘플에서 추출 - 우리가 정한 것- - 갈 수 있는 지역과 없는 지역 구분하기 위해 
 {
     Walkable,
-    Water,
+    Water, // water 프리팹 다 이걸로 지정
 }
-public class BlockInfo : MonoBehaviour
+public class BlockInfo : MonoBehaviour // 갈 수 있는지역, 없는 지역을 나누기 위한 스크립트 -
 {
-    public BlockType blockType;
+    public BlockType blockType; //자기가 어떤 블럭인지 알 수 있는
 
     Vector3 downMousePosition;
     public float clickDistance = 1; //스크린에서 최소 1 이동했다는 의미로
--- Assets/GroundManager.cs
+++ Assets/GroundManager.cs
@@ -6,10 +6,13 @@ using DG.Tweening;
 
 public class GroundManager : SingletonMonoBehavior<GroundManager>
 {
-    public Vector2Int playerPos;
+    public Vector2Int playerPos; //player 위치 
     public Dictionary<Vector2Int, int> map = new Dictionary<Vector2Int, int>();
-    public List<int> passableValues = new List<int>();// 갈 수 있는 지역
+    public List<int> passableValues = new List<int>();// 갈 수 있는 지역 //1.
     public Transform player;
+
+    //context 메뉴 ; 코르틴 실행 안됨 
+
     internal void OnTouch(Vector3 position)
     {
         Vector2Int findPos = new Vector2Int((int)position.x, (int)position.z);
@@ -26,37 +29,40 @@ public class GroundManager : SingletonMonoBehavior<GroundManager>
     IEnumerator FindPathCo(Vector2Int goalPos)
     {
         passableValues = new List<int>();
-        passableValues.Add((int)BlockType.Walkable);
+        passableValues.Add((int)BlockType.Walkable); //3. 샘플에서 값 넣는거 그대로 함
 
         //자식의 모든 블록인포 찾자
-        var blockInfos = GetComponentsInChildren<BlockInfo>(); //s 안붙여서 오류남, blockinfo 찾을수 없다고
+        var blockInfos = GetComponentsInChildren<BlockInfo>(); // 자식들에서 블록 정보를 찾는다 
+        //s 안붙여서 오류남, blockinfo 찾을수 없다고
 
         //맵을 채워넣자
-        foreach (var item in blockInfos)
+        foreach (var item in blockInfos) //뭐터랑 워커블을 이용해서 맵을 채움. 순회하며 돌기 위해 foreach 사용
         {
-            var pos = item.transform.position;
-            Vector2Int intPos = new Vector2Int((int)pos.x, (int)pos.z);
-            map[intPos] = (int)item.blockType;
-
+            var pos = item.transform.position; //아이템들의 위치값 x랑 z 값을 쓸거임
+            Vector2Int intPos = new Vector2Int((int) pos.x, (int)pos.z); //(int) 써준거는 강제로 변환해준 것)
+            map[intPos] = (int)item.blockType; //맵에 
+            //intPos가 KEY가 된다. 딕셔너리 : 앞부분키, 뒷부분 value <= 이 땅의 속성, 즉 블록타입 / 이걸 int형으로 넣어줌
+            //이렇게 해서 Map 정보 초기화가 끝났다. 
         }
         playerPos.x = (int)player.position.x;
-        playerPos.y = (int)player.position.z;
+        playerPos.y = (int)player.position.z; // 따로 써준 이유 : 우리가 쓰려는 Vecor2 값에 x, y만 있어서 y값에 z 값을 넣어줄거임
 
 
-        List<Vector2Int> path = PathFinding2D.find4(playerPos, goalPos, map, passableValues);
-        if (path.Count == 0)
+        List<Vector2Int> path = PathFinding2D.find4(playerPos, goalPos, map, passableValues); //2.
+        // Player에서 goal까지 찾겠다. / 맵 - 맵에대한 위치값, 위치, 갈수 있는지의 유무 등의 정보가 담김, / 패스 ; 갈 수 있는 지역
+        if (path.Count == 0) // path를 이용해 player가 이동하는 코드 작성
             Debug.Log("길이 없다");
         else
         {
             Player.SelectPlayer.PlayAnimation("Walk");
             FollowTarget.Instance.SetTarget(Player.SelectPlayer.transform);
-            foreach (var item in path)
+            foreach (var item in path)//path에는 길에 대한 정보만 있어서 x,y 값으로 플레이어가 갈 길 표시
             {
-                Vector3 playerNewPos = new Vector3(item.x, 0, item.y);
-                player.LookAt(playerNewPos);
-                //player.position = playerNewPos;
+                Vector3 playerNewPos = new Vector3(item.x, 0, item.y); //좌표를 변환한 것
+                player.LookAt(playerNewPos); 
+                //player.position = playerNewPos; // 좌표를 원래 플레이어한테 할당해줬었음 
                 player.DOMove(playerNewPos, moveTimePerUnit).SetEase(moveEase);
-                yield return new WaitForSeconds(moveTimePerUnit);
+                yield return new WaitForSeconds(moveTimePerUnit); 
             }
             Player.SelectPlayer.PlayAnimation("Idle");
             FollowTarget.Instance.SetTarget(null);
